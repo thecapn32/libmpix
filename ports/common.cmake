9f5fe9c2b323603444cef2daac41241e4b60c9bd
Josuah Demangeon <me@josuah.net>
2025-05-23 10:24:21 +0000

    posix: move the common CMakeLists.txt bits to a common.cmake
    This helps implementing the Zephyr port with only one list of files.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

---
+++ ports/common.cmake
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# CMake declarations common to all CMake-based ports
+
+set(LIBMPIX_SRC
+  src/formats.c
+  src/image.c
+  src/op_bayer.c
+  src/op_convert.c
+  src/op_kernel.c
+  src/op_palettize.c
+  src/op_resize.c
+  src/print.c
+  src/stats.c
+)
