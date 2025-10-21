19e196d3ff52e821acc3b3ab7a9dd2e7198d4796
thecapn32 <m.massoudi32@gmail.com>
2025-10-21 14:59:18 +0330

    feat: ccm: use cmsis-dsp vector mult for ccm

--- src/op_correct_color_matrix.c
+++ src/op_correct_color_matrix.c
@@ -1,5 +1,8 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 
+#ifdef CONFIG_MPIX_CMSIS_DSP
+#include <arm_math.h>
+#endif
 #include <mpix/low_level.h>
 #include <mpix/operation.h>
 
@@ -28,6 +31,22 @@ int mpix_add_correct_color_matrix(struct mpix_image *img, const int32_t *params)
 	return 0;
 }
 
+#ifdef CONFIG_MPIX_CMSIS_DSP
+static inline q15_t u8_to_q15(uint8_t x) {
+    return (q15_t)((uint16_t)x << 7);
+}
+
+static inline q15_t q10_to_q15(int32_t x) {
+    return (q15_t)((int32_t) x << 5);
+}
+
+static inline uint8_t q15_to_u8(q15_t q) {
+    int32_t t = (int32_t)q + 64;  // round-to-nearest
+    t >>= 7;
+    return (uint8_t)t;
+}
+#endif
+
 void mpix_correct_color_matrix_rgb24(const uint8_t *src, uint8_t *dst, uint16_t width,
 				     int32_t matrix_q10[9])
 {
@@ -41,9 +60,32 @@ void mpix_correct_color_matrix_rgb24(const uint8_t *src, uint8_t *dst, uint16_t 
 #if CONFIG_MPIX_SIMD_RVV
 	w += mpix_correct_color_matrix_raw8_rvv(src, dst, width, matrix_q10);
 #endif
+#ifdef CONFIG_MPIX_CMSIS_DSP
+    q15_t cm[9];
+    for (size_t i = 0; i < 9; i++) {
+        cm[i] = CLAMP(q10_to_q15(matrix_q10[i]), -32767, 32767);
+    }
 
-	/* Finish with the buffer with C implementation */
-	for (size_t w = 0; w + 3 <= width; w++, dst += 3, src += 3) {
+    arm_matrix_instance_q15 c;
+    arm_mat_init_q15(&c, 3, 3, cm);
+
+    for (size_t w = 0; w + 3 <= width; w++, src += 3, dst += 3) {
+        q15_t s[3];
+        s[0] = u8_to_q15(src[0]);
+        s[1] = u8_to_q15(src[1]);
+        s[2] = u8_to_q15(src[2]);
+
+        q15_t d[3];
+        arm_mat_vec_mult_q15(&c, s, d);
+
+        dst[0] = CLAMP(q15_to_u8(d[0]), 0x00, 0xff);
+        dst[1] = CLAMP(q15_to_u8(d[1]), 0x00, 0xff);
+        dst[2] = CLAMP(q15_to_u8(d[2]), 0x00, 0xff);
+    }
+#endif
+
+    /* Finish with the buffer with C implementation */
+    for (size_t w = 0; w + 3 <= width; w++, dst += 3, src += 3) {
 		int32_t r;
 		int32_t g;
 		int32_t b;
